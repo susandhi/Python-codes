9bad0f387d5b0e3697cd293b1fa6afdf31b214ca
susandhi <52783017+susandhi@users.noreply.github.com>
2019-08-11 19:13:57 +0530

    Update calculator.py
    This calculator will do basic calculation (+,-,*,/).

--- calculator.py
+++ calculator.py
@@ -1,6 +1,4 @@
 from tkinter import*
-import random
-import time
 
 root = Tk()
 root.geometry("400x500+0+0")
@@ -9,21 +7,9 @@ root.title("Calculator")
 text_Input = StringVar()
 operator = ""
 
-#Tops = Frame(root, width = 300, height = 50, bg = "powder blue", relief = SUNKEN)
-#Tops.pack(side = TOP)
-
-#f1 = Frame(root, width = 800, height = 700, bg = "powder blue", relief =SUNKEN)
-#f1.pack(side = LEFT)
-
 f2 = Frame(root, width = 300, height = 500)
 f2.pack(side = BOTTOM)
-#================Time==============================================================================================================
-localtime = time.asctime(time.localtime(time.time()))
-#====================Info==========================================================================================================
-#lblInfo = Label(Tops, font = ('arial', 50, 'bold'), text = "Basic Calculator", fg = "Steel Blue", bd = 10, anchor = 'w')
-#lblInfo.grid(row = 0, column = 0)
-#lblInfo = Label(Tops, font = ('arial', 20, 'bold'), text = localtime, fg = "Steel Blue", bd = 10, anchor = 'w')
-#lblInfo.grid(row = 1, column = 0)
+
 #====================Calculator====================================================================================================
 def btnClick(numbers):
 	global operator
